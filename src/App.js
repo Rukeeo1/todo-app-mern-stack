82f6da0ff6461bd2a42a7f9100d510a7ace3e95a
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-07-19 06:00:45 +0100

    create file for create todo, edit todo and todo list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 // import "boostrap/dist/css/boostrap.min.css"
 
 function App() {
   return (
-    <div className="container">
-      <h2 className="btn btn-danger">MERN - Stack Todo App</h2>
-    </div>
+    <Router>
+      <div className="container">
+        <h2 className="btn btn-danger">MERN - Stack Todo App</h2>
+      </div>
+      <Route path="/" exact component={TodosList} />
+      <Route path="/edit/:id" component={EditTodo} />
+      <Route path="/create" component={CreateTodo} />
+    </Router>
   );
 }
 
 export default App;
- 
