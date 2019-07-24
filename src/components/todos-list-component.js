b75eb2dc2fc1488d6601317cf97f646c2a93e101
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-07-24 23:17:46 +0100

    what's left is editing...i have been able to loop through and append the data to the page....and it's redirecting to the edit page

--- src/components/todos-list-component.js
+++ src/components/todos-list-component.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
-import { link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
+
+const Todo = (
+  props //this is functional component...
+) => (
+  <tr>
+    {console.log(props)}
+    <td>{props.todo.todo_description}</td>
+    <td>{props.todo.todo_responsible}</td>
+    <td>{props.todo.todo_priority}.</td>
+    <td>
+      <Link to={'/edit/' + props.todo._id}>Edit</Link>
+    </td>
+  </tr>
+);
 
 class TodoList extends Component {
   constructor(props) {
@@ -14,16 +28,38 @@ class TodoList extends Component {
     axios
       .get('http://localhost:3000/todos')
       .then(response => {
-        this.setState({ todos: response.data });
+        this.setState({ todo: response.data });
       })
       .catch(error => {
         console.log(error);
       });
   }
+
+  todoList() {
+    //this is a function...
+    //the function loops throught the todo list...
+    //and returns a todo functional component at each loop
+    //it passes a prop at each time
+    //the prop is an object of the current to do...
+    return this.state.todo.map((currentTodo, index) => {
+      return <Todo todo={currentTodo} key={index} />;
+    });
+  }
   render() {
     return (
       <div>
-        <p>Welcome to to do list todoComponent</p>
+        <h3>Todos List</h3>
+        <table className="table table-striped" style={{ marginTop: 20 }}>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>response</th>
+              <th>Priority</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{this.todoList()}</tbody>
+        </table>
       </div>
     );
   }
